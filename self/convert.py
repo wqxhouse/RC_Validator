b2c75a5d37ec0e051c733f2e6dd4fefc7b5a86f7
wqxhouse <wqxhouse1@gmail.com>
2014-06-05 19:07:53 -0700

    added phase 3 constrcuts

--- self/convert.py
+++ self/convert.py
@@ -6,6 +6,7 @@ os.chdir(".")
 # global vars
 isVoidMain = False;
 param_array_dict = {};
+ptr_des_dict = {};
 
 def isAllUpperCase(str) :
 	for l in str :
@@ -29,14 +30,139 @@ def process(str) :
 	str = process_tf(str);
 	str = process_void_main(str);
 	str = process_cout_cin_expr(str);
+
 	str = process_sizeof_param(str);
+	str = change_nullptr(str);
+	save_ptr_des(str);
+	str = process_new(str);
+	str = process_funcptr(str);
+#	str = substitute_bool(str);
+#	str = process_cout_bool(str);
 	
+	str = process_cout_float(str);	
+	return str;
+	
+def process_funcptr(str) :
+	if "funcptr" in str :
+		optInit = "";
+		temp = "";
+		if '=' in str :
+			temp = str[: str.find('=')].strip();
+			optInit = str[str.find('=')+1 : str.find(';')].strip();
+		else :
+			temp = str[: str.find(';')].strip();
+			
+		arr = temp.split();
+		des = arr[-1];
+		
+		retType = temp[temp.find(':')+1 : temp.find('(')].strip();
+		argStr = temp[temp.find('(')+1 : temp.find(')')];
+		argList = argStr.split(',');
+		argTypeList = [];
+		if len(argList) != 1 :
+			for e in argList :
+				st = e.strip();
+				starr = st.split();
+				argTypeList.append(starr[0]);
+		
+		str = '    ' + retType + " (*" + des + ")( ";
+
+		for e in argTypeList :
+			str += e + ", ";
+
+		if len(argTypeList) != 0 :
+			str = str.rstrip().rstrip(',');
+		str += ")";
+		
+		if optInit != "" :
+			str += " = " + optInit;
+			
+		str += ";\n"
+		
+	return str;
+	
+def change_nullptr(str) :
+	if "nullptr" in str :
+		str = str.replace("nullptr", "0");
+	return str;
+	
+def save_ptr_des(str) :
+	global ptr_des_dict;
+	
+	if "*" in str :
+		str = str.replace(" ", "");
+		if ',' in str : 
+			itemList = str.split(',');
+			for e in itemList :
+				if "*" in e :
+					strarr = e.split('*');
+					type = strarr[0].replace(" ", "");
+					if '(' in type :
+						type = type[type.find('(') + 1 :];
+					elif ')' in type :
+						type = type[: type.find(')')];
+					des = strarr[1].replace(" ", "");
+					if ";" in des :
+						des = des[: des.find(";") ];
+					ptr_des_dict[des] = type;
+
+		else :	
+			strarr = str.split('*');
+			type = strarr[0].replace(" ", "");
+			if '(' in type :
+				type = type[type.find('(') + 1 :];
+			elif ')' in type :
+				type = type[: type.find(')')];
+			des = strarr[1].replace(" ", "");
+			if ";" in des :
+				des = des[: des.find(";") ];
+			ptr_des_dict[des] = type;
+			
+def substitute_bool(str) :
+	if 'bool' in str :
+		str = str.replace('bool', 'Bool');
+	return str;
+			
+def process_cout_bool(str) :
+	if "cout" in str :
+		str = str.replace(" ", "");
+		coutarr = str.split('<<');
+
+		count = 0;
+		for e in coutarr :
+			if (e != "cout") and (e != "endl") and (e != "endl;\n") and ("\"" not in e) :
+				coutarr[count] = "Bool::toString(" + e + ") << ";
+			elif (e == "cout") or (e == "endl") :
+				coutarr[count] = e + " << ";
+
+			count += 1;
+			
+		str = "".join(coutarr);
+	return str;
+		
+def process_new(str) :
+	res = str;
+	if "new" in str :
+		des = str[str.find("new") + 3 :];
+		if ";" in des :
+			des = des[: des.find(";") ];
+		des = des.replace(" ", "");
+		res = '\t' + des + " = ";
+		if des in ptr_des_dict :		
+			res += "new " + ptr_des_dict[des] + ";\n";		
+	return res;
+
+def process_cout_float(str) :
+	if "cout" in str :
+		str = str[: str.find('<<')] + " << std::fixed << std::setprecision(2) " +  str[str.find('<<') :];
 	return str;
 	
 def format_elem_coutcin(str) :
 	if ";" in str and "endl" not in str:
 		return "(" + str[: str.find(";")] + ")" + str[str.find(";") :];
 	elif ";" in str and "endl" in str:
+		return str;
+	elif "endl" in str :
 		return str;
 	else :
 		return "(" + str + ")"
@@ -48,21 +174,8 @@ def process_cout_cin_expr(str) :
 		counter = 0;
 		cout_or_cin = -1; # 0 is cout ; 1 is cin
 
-		#if len(exprarr) == 2 and ("endl" in exprarr[1] ) :
-			#if "cout" in exprarr[0] :
-				#exprarr.insert(1, "<<");
-			#elif "cin" in exprarr[0] :
-				#exprarr.insert(1, ">>");
-		#else :
 		while True:
 			elem = exprarr[counter];
-			#if (elem == "<<" or elem == ">>") and \
-			#   (counter + 2) < len(exprarr) and \
-			#   (exprarr[counter+2] == "<<" or exprarr[counter+2] == ">>") :
-			   
-			#	str = exprarr[counter+1];
-			#	str = "(" + str + ")";
-			#	exprarr[counter+1] = str;
 			if "cout" not in elem and "cin" not in elem  :	
 				if "endl" in elem :
 					exprarr[counter] = " << " + format_elem_coutcin(elem);
@@ -121,7 +234,6 @@ def process_tf(str) :
 		str = str.replace("true", "1");
 	if "false" in str :
 		str = str.replace("false", "0");
-		
 	return str;
 	
 def process_void_main(str) :
@@ -205,9 +317,14 @@ for filename in glob.glob("*.rc"):
 	file = open(filename, 'r');
 	write = open(filename + ".cpp", "w");
 	write.write("#include <iostream>\n");
+	write.write("#include <stdlib.h>\n");
+	write.write("#include <iomanip>\n");
+	write.write("#include <string.h>\n");
 	write.write("using namespace std;\n");
+
 	count = 0;
 	for line in file :
+		#line = line.strip();
 		if line not in ['\n', '\r\n'] : 
 			line = process(line);
 			write.write(line);
@@ -219,7 +336,7 @@ for filename in glob.glob("*.rc"):
 		contents = write.readlines();
 		write.close();
 
-		contents.insert(count-1+2, "   return 0;\n");
+		contents.insert(count-1+5, "   return 0;\n");
 		contents = "".join(contents);
 
 		write = open(filename + ".cpp", "w");
